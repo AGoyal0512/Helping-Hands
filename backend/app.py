fec8be1b6a5ae52c44c0d27f3a31cbcf57cb40cc
shivanshagg18 <90158094+shivanshagg18@users.noreply.github.com>
2021-10-17 03:16:09 -0500

    Updated app.py with email feature

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, request
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
-mail= Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'shivanshagg18@gmail.com'
-app.config['MAIL_PASSWORD'] = 'GAInTheHouse'
+app.config['MAIL_USERNAME'] = 'HelpingHands00018@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Datathon_18'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
@@ -26,8 +25,13 @@ def assess():
 
 @app.route("/form", methods=['POST'])
 def post():
-    print(request.form.get("First"))
-    print(request.form.get("Last"))
+    first = request.form.get("First")
+    last = request.form.get("Last")
+    subject = "Patient: " + first + " " + last
+    msg = Message(subject, sender = 'HelpingHands0018@gmail.edu', recipients = [request.form.get("email")])
+    msg.body = "Patient's number: " + request.form.get("Phone")
+    msg.body = msg.body + "\n" + "Patient's response: " + request.form.get("text")
+    mail.send(msg)
     return render_template("assess.html")
 
 app.run(port=5000)
